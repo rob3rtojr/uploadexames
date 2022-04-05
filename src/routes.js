da537c8cf39508fb822433cbd03d835744860d63
Roberto <roberto@rssolucoesweb.com.br>
2022-04-05 09:44:14 -0300

    teste

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ const multerConfig = require('./config/multer');
 
 const routes = express.Router();
 
-routes.get('/',(req, res)=> res.send('upload de exames doutorweb running! receba!'))
+routes.get('/',(req, res)=> res.send('upload de exames doutorweb running! 05/04/2022'))
 routes.get('/testes', TesteController.index)
 routes.post('/testes', multer(multerConfig).single("file"), TesteController.store)
 routes.delete('/testes/:id', TesteController.delete)
