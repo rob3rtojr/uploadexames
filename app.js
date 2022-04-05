ba52385b3b31af7c5ad4fec3b3e5a86eaca234ab
Roberto <roberto@rssolucoesweb.com.br>
2022-04-04 23:39:00 -0300

    testes implantação

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
-//app.use(require('./src/routes'));
-
+app.use(require('./src/routes'));
 
 app.listen(port,()=>{
     console.log('JSON Server is running - port: ' + port);
--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ const multerConfig = require('./config/multer');
 
 const routes = express.Router();
 
-routes.get('/',(req, res)=> res.send('upload de exames doutorweb running'))
+routes.get('/',(req, res)=> res.send('upload de exames doutorweb running! receba!'))
 routes.get('/testes', TesteController.index)
 routes.post('/testes', multer(multerConfig).single("file"), TesteController.store)
 routes.delete('/testes/:id', TesteController.delete)
